1588569064e62faa0d98968a3e95e319a8fccd6c
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-06-28 12:52:15 +0000

    measurement typo fixed in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Context } from './Context';
 export { default as Interests } from './Interests';
-export { default as Measurements } from './Measurements';
+export { default as Measurement } from './Measurement';
 export { default as Start } from './Start';
 export { default as True } from './True';
 export { default as Welcome } from './Welcome';
